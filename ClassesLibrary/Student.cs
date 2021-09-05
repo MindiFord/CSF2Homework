2b8c5c1843d6b1b64f304a5976aba3971e8563a4
Mindi Ford <MindiFord@live.com>
2021-09-04 20:09:00 -0500

    Student complete

---
+++ ClassesLibrary/Student.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassesLibrary
+{
+    public class Student
+    {
+        //fields
+        private string _firstName;
+        private string _lastName;
+        private string _id;
+        private float _gpa;
+
+        //properties
+        public string FirstName
+        {
+            get { return _firstName; }
+            set { _firstName = value; }
+        }
+
+        public string LastName
+        {
+            get { return _lastName; }
+            set { _lastName = value; }
+        }
+
+        public string Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public float Gpa
+        {
+            get { return _gpa; }
+            set { _gpa = value; }
+        }
+
+        //ctors
+        //FQ
+        public Student(string firstName, string lastName, string id, float gpa)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Id = id;
+            Gpa = gpa;
+        }
+        //default
+        public Student()
+        {
+
+        }
+
+        //methods
+        public override string ToString()
+        {
+            return string.Format($"" +
+                $"First Name: {FirstName}\n" +
+                $"Last Name: {LastName}\n" +
+                $"Id: {Id}\n" +
+                $"GPA: {Gpa}\n");
+        }
+
+    }
+}
--- TesterProgram/Program.cs
+++ TesterProgram/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ClassesLibrary;
 
 namespace TesterProgram
 {
@@ -10,6 +11,19 @@ namespace TesterProgram
     {
         static void Main(string[] args)
         {
+            Student s1 = new Student();
+
+            s1.FirstName = "Johnny";
+            s1.LastName = "Castle";
+            s1.Id = "1963";
+            s1.Gpa = 2.9f;
+
+            Console.WriteLine(s1);
+
+            Student s2 = new Student("Vida", "Boheme", "1995", 3.8f);
+
+            Console.WriteLine(s2);
+
         }
     }
 }
