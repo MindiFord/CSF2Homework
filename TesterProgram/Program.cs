d33b66b46be3859a27ad4c94aa29b1a76522c996
Mindi Ford <MindiFord@live.com>
2021-09-04 20:36:24 -0500

    Vehicle complete

--- TesterProgram/Program.cs
+++ TesterProgram/Program.cs
@@ -24,6 +24,21 @@ namespace TesterProgram
 
             Console.WriteLine(s2);
 
+            Console.WriteLine("\n\n\n");
+
+            Vehicle v1 = new Vehicle();
+
+            v1.Make = "Triumph";
+            v1.Model = "Mayflower";
+            v1.Year = 1949;
+            v1.Weight = 2050f;
+
+            Console.WriteLine(v1);
+
+            Vehicle v2 = new Vehicle("Mercury", "Bobcat", 1974, 2300);
+
+            Console.WriteLine(v2);
+
         }
     }
 }
